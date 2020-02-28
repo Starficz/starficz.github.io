9c9d9a6ab287213f9102167846ae8c786b5da2b3
Starficz <starficz@gmail.com>
2020-02-28 00:52:38 -0500

    Integrated Template

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 functionPlot({
-  target: root,
-  yAxis: { domain: [-1, 9] },
-  tip: {
-    renderer: function() {}
-  },
-  grid: true,
-  data: [
-    {
-      fn: "x^2",
-      derivative: {
-        fn: "2 * x",
-        updateOnMouseMove: true
-      }
-    }
-  ]
+	target: root,
+	xAxis: { label: 'Armour', domain: [0, 3000] },
+	yAxis: { label: 'DPS', domain: [0, 3000] },
+	tip: {
+		renderer: function() {}
+	},
+	grid: false,
+	disableZoom: true,
+	data: [
+		{
+			fn: "2000 - x",
+		}
+	],
+	annotations: [
+		{
+			x: 100,
+			text: 'Basic Solider',
+			color: 'red'
+		},
+		{
+			x: 1000,
+			text: 'Armored Heavy',
+			color: 'red'
+		}
+	]
 });
